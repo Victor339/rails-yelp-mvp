ec64f0c2ca495608abd17b61322ab5650db9a13c
Victor Bolta <victor.bolta@icloud.com>
2020-08-12 15:56:12 +0200

    Rails short mvp

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,12 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   movies = Movie.create( name: 'Star Wars' }, name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.create(name: 'Panda Panda', address: '21 rue Juliette Dodu', category: 'chinese')
+Restaurant.create(name: 'Happa Tei', address: '10 rue Saint Anne', category: 'japanese')
+Restaurant.create(name: 'Ober Mamma', address: '30 rue Oberkampf', category: 'italian')
+Restaurant.create(name: 'La Véraison', address: '67 rue de la Croix Nivert', category: 'french')
+Restaurant.create(name: "L'Estaminet", address: '33 rue de Navarin', category: 'belgian')
